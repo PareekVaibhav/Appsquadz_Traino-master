c4b68753d318a4efaba20d430e8a0395e994b365
PareekVaibhav <vaibhavpareek34@gmail.com>
2019-06-20 23:06:14 +0530

    initail commit

---
+++ app/src/main/java/com/appsquadz/appsquadz_traino/DayTwoActivity.java
@@ -0,0 +1,31 @@
+package com.appsquadz.appsquadz_traino;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DayTwoActivity extends AppCompatActivity {
+
+    List<String> data = new ArrayList<>();
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_two);
+        context = this;
+
+        for (int i = 0; i < 20; i++)
+            data.add("Data : " + i);
+
+        ListView myList = findViewById(R.id.my_list);
+        ArrayAdapter adapter = new ArrayAdapter(context, R.layout.list_text, data);
+        myList.setAdapter(adapter);
+
+    }
+}
